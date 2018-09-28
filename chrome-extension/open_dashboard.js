558aa565299b0bf6db3afeb9018c11554a3b072d
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-09-28 14:03:06 -0700

    scripts

---
+++ chrome-extension/open_dashboard.js
@@ -0,0 +1,18 @@
+// called when the user clicks on the browser action
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // send a "message" to the active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        const activeTab = tabs[0]
+        chrome.tabs.sendMessage(activeTab.id, { event: 'click' })
+    })
+})
+  
+// open a new tab to a given URL
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        const { event, url } = request;
+        if (event === 'open_dashboard') {
+            chrome.tabs.create({url, active: true});
+        }
+    }
+)
---
+++ chrome-extension/parser.js
@@ -0,0 +1,52 @@
+const HOST_PROD = 'http://dashboard.cbtn-tools-prod.local';
+const HOST_QA = 'http://dashboard.cbtn-tools-qa.local';
+const ROUTE = '/training/submit';
+const BASE_URL = `${HOST_PROD}${ROUTE}`;
+
+function getPageTitle() {
+    // opengraph first because of github repos' metadata
+    let title = $("meta[property='og:title']").attr('content');
+
+    // header
+    if (!title) {
+        title = $("title").html();
+    }
+
+    // meta
+    if (!title) {
+        title = $("meta[name='title']").attr('content');
+    }
+
+    // twitter
+    if (!title) {
+        title = $("meta[name='twitter:title']").attr('content');
+    }
+
+    return title;
+}
+
+function encode(string) {
+    return encodeURIComponent(string);
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        const { event } = request;
+        if (event === 'click') {
+            console.log('parsing current page...');
+
+            const url = document.location.href;
+            let title = getPagetitle();
+            console.log({ url, title });
+
+            const parameters =
+                `?url=${encode(url)}` +
+                `&title=${encode(title)}`;
+
+            const url = `${BASE_URL}${parameters}`;
+            console.log(`new tab will open at '${url.replace(/\s/g,'%20')}'`);
+
+            chrome.runtime.sendMessage({ event: 'open_dashboard', url });
+        }
+    }
+);
