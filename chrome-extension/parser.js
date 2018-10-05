366d3ab4f8420e1cbd3124561105cc6162bb6f3f
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-10-05 16:29:48 -0700

    sanitize input

--- chrome-extension/parser.js
+++ chrome-extension/parser.js
@@ -2,7 +2,7 @@ const HOST_PROD = 'http://dashboard.cbtn-tools-prod.local';
 const HOST_QA = 'http://dashboard.cbtn-tools-qa.local';
 const HOST_DEV = 'http://localhost:8080';
 const ROUTE = '/training/submit';
-const BASE_URL = `${HOST_QA}${ROUTE}`;
+const BASE_URL = `${HOST_DEV}${ROUTE}`;
 
 function getTopic() {
     // opengraph first because of github repos' metadata
@@ -30,6 +30,14 @@ function encode(string) {
     return encodeURIComponent(string);
 }
 
+function getTrainedFor(trainedFor) {
+    if (isNaN(trainedFor)) {
+        return 0;
+    }
+
+    return trainedFor * 60;
+}
+
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         const { event } = request;
@@ -38,16 +46,27 @@ chrome.runtime.onMessage.addListener(
 
             const link = document.location.href;
             let topic = getTopic();
-            console.log({ link, topic });
 
-            const parameters =
+            chrome.storage.sync.get({
+                trainedFor: 0,
+                rating: 0,
+            }, function(items) {
+                const { trainedFor, rating } = items;
+                console.log({ link, topic });
+
+                const parameters =
                 `?topic=${encode(topic)}` + 
-                `&link=${encode(link)}`;
+                `&link=${encode(link)}` + 
+                `&trainedFor=${encode(getTrainedFor(trainedFor))}` + 
+                `&rating=${encode(rating)}`;
 
-            const dashboardUrl = `${BASE_URL}${parameters}`;
-            console.log(`new tab will open at '${dashboardUrl.replace(/\s/g,'%20')}'`);
+                const dashboardUrl = `${BASE_URL}${parameters}`;
+                console.log(`new tab will open at '${dashboardUrl.replace(/\s/g,'%20')}'`);
 
-            chrome.runtime.sendMessage({ event: 'open_dashboard', url: dashboardUrl });
+                chrome.runtime.sendMessage({ event: 'open_dashboard', url: dashboardUrl });
+            });
+
+
         }
     }
 );
